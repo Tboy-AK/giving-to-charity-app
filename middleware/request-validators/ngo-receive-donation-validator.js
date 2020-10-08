04f14a80657b2ee3862fb94ac6fa0222d5c2869b
Tobi Akanji <36475654+Tboy-AK@users.noreply.github.com>
2020-10-08 15:56:43 +0100

    ft(ngo-receive-donation):ngo donation[Finishes #58]

---
+++ middleware/request-validators/ngo-receive-donation-validator.js
@@ -0,0 +1,38 @@
+const { param, header } = require('express-validator');
+const { mongoose: { Types } } = require('../../configs/mongodb-config');
+
+// GET /ngo/:ngoId/donation/:donationId/received
+// GET /event/:eventId/donation/:donationId/received
+const validators = [
+  header('useraccesspayload')
+    .notEmpty()
+    .withMessage('Not authorized')
+    .custom((val) => (
+      Types.ObjectId(val.authId)
+      && Types.ObjectId(val.authId).toHexString() === val.authId
+    ))
+    .withMessage('Invalid access'),
+  param('ngoId')
+    .if((val, { req }) => !(
+      !val && !req.eventId
+    ) && !req.eventId)
+    .notEmpty()
+    .withMessage('NGO ID is required')
+    .isMongoId()
+    .withMessage('Invalid NGO ID'),
+  param('eventId')
+    .if((val, { req }) => !(
+      !val && !req.ngoId
+    ) && req.ngoId)
+    .notEmpty()
+    .withMessage('Event ID is required')
+    .isMongoId()
+    .withMessage('Invalid Event ID'),
+  param('donationId')
+    .notEmpty()
+    .withMessage('Donation ID is required')
+    .isMongoId()
+    .withMessage('Invalid donation ID'),
+];
+
+module.exports = validators;
--- routers/ngo-donation-router.js
+++ routers/ngo-donation-router.js
@@ -2,11 +2,14 @@ const { Router } = require('express');
 const ngoDonationValidator = require('../middleware/request-validators/ngo-donation-validator');
 const ngoListDonationValidator = require('../middleware/request-validators/ngo-list-donation-validator');
 const ngoViewDonationValidator = require('../middleware/request-validators/ngo-view-donation-validator');
+const ngoReceiveDonationValidator = require('../middleware/request-validators/ngo-receive-donation-validator');
 const authNgo = require('../middleware/auth-ngo-middleware');
 const errResponse = require('../utils/error-response-handler');
 const DonationModel = require('../models/mongodb-models/donation-model');
 const NGOModel = require('../models/mongodb-models/ngo-model');
-const { donateItem, listDonations, viewDonation } = require('../controllers/ngo-donation-controller')(errResponse, DonationModel, NGOModel);
+const {
+  donateItem, listDonations, viewDonation, receiveDonation,
+} = require('../controllers/ngo-donation-controller')(errResponse, DonationModel, NGOModel);
 
 const NGODonationRouter = Router();
 
@@ -18,5 +21,9 @@ NGODonationRouter
 NGODonationRouter
   .route('/ngo/:ngoId/donation/:donationId')
   .get(authNgo, ngoViewDonationValidator, viewDonation);
+
+NGODonationRouter
+  .route('/ngo/:ngoId/donation/:donationId/received')
+  .patch(authNgo, ngoReceiveDonationValidator, receiveDonation);
 
 module.exports = { NGODonationRouter };
