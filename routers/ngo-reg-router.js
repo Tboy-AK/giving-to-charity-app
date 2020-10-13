9a83bb0e7f8c86fd4aaaa61cca170d3dd72674e5
Tobi Akanji <36475654+Tboy-AK@users.noreply.github.com>
2020-10-13 23:55:17 +0100

    ft(user-get-ngo):[Finishes #98 #99]
    - User can list all verified NGOs
    - User can view publicly accessible details of an NGO

--- routers/ngo-reg-router.js
+++ routers/ngo-reg-router.js
@@ -2,16 +2,25 @@ const { Router } = require('express');
 const authAdmin = require('../middleware/auth-admin-middleware');
 const ngoRegValidator = require('../middleware/request-validators/ngo-reg-validator');
 const ngoVerifyValidator = require('../middleware/request-validators/ngo-verify-validator');
+const userListNGOsValidator = require('../middleware/request-validators/user-list-ngos-validator');
+const userViewNGOValidator = require('../middleware/request-validators/user-view-ngo-validator');
 const errResponse = require('../utils/error-response-handler');
 const AuthModel = require('../models/mongodb-models/auth-model');
 const NGOModel = require('../models/mongodb-models/ngo-model');
-const { createNGO, adminVerifyNGO } = require('../controllers/ngo-reg-controller')(errResponse, AuthModel, NGOModel);
+const {
+  createNGO, adminVerifyNGO, listNGOs, viewNGO,
+} = require('../controllers/ngo-reg-controller')(errResponse, AuthModel, NGOModel);
 
 const NGORegRouter = Router();
 
 NGORegRouter
   .route('/ngo')
+  .get(userListNGOsValidator, listNGOs) // User can list all NGOs
   .post(ngoRegValidator, createNGO); // NGO can register
+
+NGORegRouter
+  .route('/ngo/:ngoId')
+  .get(userViewNGOValidator, viewNGO); // User can view an NGO
 
 NGORegRouter
   .route('/ngo/:ngoId/verify')
