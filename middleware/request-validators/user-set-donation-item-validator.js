d787253dd0ec480c9e4c1baebc93fa27ea21ca8b
Tobi Akanji <36475654+Tboy-AK@users.noreply.github.com>
2020-10-29 03:45:20 +0100

    bg(user-donation-items):[Finishes #102]

--- middleware/request-validators/user-set-donation-item-validator.js
+++ middleware/request-validators/user-set-donation-item-validator.js
@@ -1,32 +1,40 @@
 const { body } = require('express-validator');
 
 const validators = [
+  body()
+    .isArray({ min: 1 })
+    .withMessage('Request body must be an array'),
   body('*.name')
     .isString()
+    .withMessage('Name must be text')
     .trim(' ')
     .notEmpty()
     .withMessage('Item name is required')
     .isLength({ min: 2, max: 30 })
-    .withMessage('Item name must be between 2 and 30 characters long'),
+    .withMessage('Item name must be between 2 and 30 characters long')
+    .escape(),
   body('*.quantity')
     .isInt({ min: 1 })
-    .withMessage('Quantity must be a whole non-zero number')
-    .escape(),
+    .withMessage('Quantity must be a whole non-zero number'),
   body('*.desc')
     .isString()
+    .withMessage('Description must be text')
     .trim(' ')
     .notEmpty()
     .withMessage('Descriptions make items much clearer')
     .isLength({ min: 12, max: 100 })
-    .withMessage('Description must be between 12 and 100 character length'),
+    .withMessage('Description must be between 12 and 100 character length')
+    .escape(),
   body('*.purpose')
     .optional()
     .isString()
+    .withMessage('Purpose must be text')
     .trim(' ')
     .notEmpty()
     .withMessage('Purpose cannot be empty if present')
     .isLength({ min: 12, max: 100 })
-    .withMessage('Purpose must be between 12 and 100 character length'),
+    .withMessage('Purpose must be between 12 and 100 character length')
+    .escape(),
 ];
 
 module.exports = validators;
